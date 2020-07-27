e7e8da25e3bacc986ccb20aa504bd0b8ec178827
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-07-27 07:59:35 -0700

    Add files via upload

---
+++ amount.java
@@ -0,0 +1,37 @@
+class amount
+{
+public static void main(String jk[])
+{
+int amt=7583,a,b,c,d,e,f,g,h,i;
+a=amt/1000;
+amt=amt%1000;
+b=amt/500;
+amt=amt%500;
+c=amt/100;
+amt=amt%100;
+d=amt/50;
+amt=amt%50;
+e=amt/20;
+amt=amt%20;
+f=amt/10;
+amt=amt%10;
+g=amt/5;
+amt=amt%5;
+h=amt/2;
+amt=amt%2;
+i=amt/1;
+amt=amt%1;
+System.out.println("1000 Notes="+a);
+System.out.println("500 Notes="+b);
+System.out.println("100 Notes="+c);
+System.out.println("50 Notes="+d);
+System.out.println("20 Notes="+e);
+System.out.println("10 Notes="+f);
+System.out.println("5 Notes="+g);
+System.out.println("2 Notes="+h);
+System.out.println("1 Notes="+i);
+}
+}
+
+
+
